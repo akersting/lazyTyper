b6ca9bbda09a20b4bd4420e5debea8faf4087e0d
Andreas Kersting <andreas@akersting.de>
2016-10-08 00:15:59 +0200

    getLHSVariableName: accept string literals as LHS
    this mimics the behavior of the base assignment operator

--- R/getLHSVariableName.R
+++ R/getLHSVariableName.R
@@ -4,6 +4,9 @@
 #' the variable which would be created or modified by a \emph{valid} assignment.
 #'
 #' @param x the left-hand side of an assignment as a language object.
+#' @param allow_character a logical value indicating if \code{"abc"} or even
+#'   \code{"123"} (but not \code{""}) are acceptable left-hand sides.
+#'   \code{TRUE} mimics the behavior of the base assignment operator.
 #'
 #' @return a character string.
 #'
@@ -13,14 +16,16 @@
 #' assignment <- quote(names(var)[1:3] <- letters[1:3])
 #' lhs <- assignment[[2]]  # left-hand side of assignment
 #' lazyTyper:::getLHSVariableName(lhs)  # "var"
-getLHSVariableName <- function(x) {
+getLHSVariableName <- function(x, allow_character = TRUE) {
   if (is.name(x)) {
     return(as.character(x))
-  } else if (is.call(x) && is.name(x[[2]])) {
+  } else if (is.call(x) && length(x) > 1 && is.name(x[[2]])) {
     return(as.character(x[[2]]))
   } else if (is.call(x) && (identical(x[[1]], quote(`[`)) ||
                             identical(x[[1]], quote(`[[`)))) {
-    return(getLHSVariableName(x[[2]]))
+    return(getLHSVariableName(x[[2]], allow_character = FALSE))
+  } else if (allow_character && is.character(x) && nchar(x) > 0) {
+    return(x)
   } else {
     stop("Invalid left-hand side to assignment.")
   }
