7627bab35c0e44d8a6b40c9077056ef5b9bf3379
Andreas Kersting <andreas@akersting.de>
2016-11-21 20:21:12 +0100

    RStudio Addins: auto-spacing of assignment ops

--- R/rstudioAddin.R
+++ R/rstudioAddin.R
@@ -1,32 +1,63 @@
 # nocov start
 
-shiftCursorInLine <- function(offset) {
-  adc <- rstudioapi::getActiveDocumentContext()
+shiftCursorInLine <- function(offset, adc) {
   adc$selection[[1]]$range$start[2] <- adc$selection[[1]]$range$start[2] +
     offset
   adc$selection[[1]]$range$end[2] <- adc$selection[[1]]$range$end[2] + offset
   rstudioapi::setCursorPosition(adc$selection[[1]]$range)
 }
 
+isSpace2Left <- function(adc) {
+  line <- adc$contents[adc$selection[[1]]$range$start[1]]
+  char2left <- substr(line, adc$selection[[1]]$range$start[2] - 1,
+                      adc$selection[[1]]$range$start[2] - 1)
+  if (nchar(char2left) > 0) {
+    length(grep("[[:space:]]", char2left)) > 0
+  } else {
+    TRUE  # we are a the very beginning of a line
+  }
+}
+
 insertTypedAssignAddin <- function() {
-  rstudioapi::insertText(" %<-% .()")
-  shiftCursorInLine(-1)
+  adc <- rstudioapi::getActiveDocumentContext()
+  if (isSpace2Left(adc)) {
+    rstudioapi::insertText("%<-% .()")
+    # 8 is nchar("%<-% .()")
+    adc$selection[[1]]$range$start[2] <- adc$selection[[1]]$range$start[2] + 8
+    adc$selection[[1]]$range$end[2] <- adc$selection[[1]]$range$start[2]
+  } else {
+    # 9 is nchar(" %<-% .()")
+    rstudioapi::insertText(" %<-% .()")
+    adc$selection[[1]]$range$start[2] <- adc$selection[[1]]$range$start[2] + 9
+    adc$selection[[1]]$range$end[2] <- adc$selection[[1]]$range$start[2]
+  }
+
+  shiftCursorInLine(-1, adc)
 }
-insertTypedAssignWOSpaceAddin <- function() {
-  rstudioapi::insertText("%<-% .()")
-  shiftCursorInLine(-1)
-}
+
 insertSecureTypedAssignAddin <- function() {
-  rstudioapi::insertText(" %<-s% .()")
-  shiftCursorInLine(-1)
-}
-insertSecureTypedAssignWOSpaceAddin <- function() {
-  rstudioapi::insertText("%<-s% .()")
-  shiftCursorInLine(-1)
+  adc <- rstudioapi::getActiveDocumentContext()
+  if (isSpace2Left(adc)) {
+    rstudioapi::insertText("%<-s% .()")
+    # 9 is nchar("%<-s% .()")
+    adc$selection[[1]]$range$start[2] <- adc$selection[[1]]$range$start[2] + 9
+    adc$selection[[1]]$range$end[2] <- adc$selection[[1]]$range$start[2]
+  } else {
+    rstudioapi::insertText(" %<-s% .()")
+    # 10 is nchar(" %<-s% .()")
+    adc$selection[[1]]$range$start[2] <- adc$selection[[1]]$range$start[2] + 10
+    adc$selection[[1]]$range$end[2] <- adc$selection[[1]]$range$start[2]
+  }
+
+  shiftCursorInLine(-1, adc)
 }
 
 insertScopeAddin <- function() {
+  adc <- rstudioapi::getActiveDocumentContext()
   rstudioapi::insertText("SCOPE %->% {}")
-  shiftCursorInLine(-1)
+  # 13 is nchar("SCOPE %->% {}")
+  adc$selection[[1]]$range$start[2] <- adc$selection[[1]]$range$start[2] + 13
+  adc$selection[[1]]$range$end[2] <- adc$selection[[1]]$range$start[2]
+  shiftCursorInLine(-1, adc)
 }
 # nocov end
