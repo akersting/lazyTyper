b3a47bcab98a354127124a873328829ad9f75c9e
Andreas Kersting <andreas@akersting.de>
2017-06-06 00:39:43 +0200

    fix: tests of remove failed if triggered by covr

--- tests/testthat/test_remove.R
+++ tests/testthat/test_remove.R
@@ -1,15 +1,24 @@
 context("remove/rm")
 
 test_that("remove/rm does not unexpectately differ from base remove/rm", {
-  expect_equal(as.character(body(remove))[-(7:11)],
-               as.character(body(base::remove))[-7])
-  expect_equal(as.character(body(rm))[-c(7:11)],
-               as.character(body(base::rm))[-7])
-})
+  A <- as.character(body(base::remove))[-7]
+  B <- gsub("\n[ ]*", "", as.character(body(remove))[-(7:11)])
+  for (i in seq_along(A)) {
+    expect_equal(grep(A[i], B[i], fixed = TRUE), 1)
+  }
 
-test_that("code of remove and rm is identical", {
-  expect_equal(as.character(body(rm)),
-               as.character(body(remove)))
+  A <- as.character(body(base::rm))[-7]
+  B <- gsub("\n[ ]*", "", as.character(body(rm))[-(7:11)])
+  for (i in seq_along(A)) {
+    expect_equal(grep(A[i], B[i], fixed = TRUE), 1)
+  }
+
+  # fails if run by covr:
+
+  # expect_equal(as.character(body(remove))[-(7:11)],
+  #              as.character(body(base::remove))[-7])
+  # expect_equal(as.character(body(rm))[-c(7:11)],
+  #              as.character(body(base::rm))[-7])
 })
 
 test_that("remove does not untype non-existing variables", {
